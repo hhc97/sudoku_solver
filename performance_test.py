6dac9ec036f5f1e3f6493cb271888745a2feae81
Haocheng Hu <hc.hu@utoronto.ca>
2021-02-22 01:06:41 -0500

    add relative performance indicator

--- performance_test.py
+++ performance_test.py
@@ -3,9 +3,25 @@ A module that tests the performance of the solver by getting it to solve a set o
 Solver correctness is not checked, only the solve times are recorded.
 """
 
+from time import time
 from datetime import datetime
 
-from sudoku_solver import SudokuPuzzle, depth_first_solve, time
+from sudoku_solver import SudokuPuzzle, depth_first_solve
+
+
+def measure_cpu_speed(num: int) -> float:
+    """
+    Estimates the average CPU speed by measuring how fast the CPU can count.
+    """
+
+    def _measure() -> int:
+        count = 0
+        start = time()
+        while time() - start < 1:
+            count += 1
+        return count
+
+    return sum(_measure() for _ in range(num)) / num
 
 
 def test_performance(filename: str) -> None:
@@ -13,6 +29,7 @@ def test_performance(filename: str) -> None:
     Gets the solver to solve puzzles from a text file, and prints the statistics.
     """
     symbol_set, times = {str(i) for i in range(1, 10)}, []
+    speed_begin = measure_cpu_speed(5)
     with open(filename) as data:
         for line in data:
             line = line.strip('\n').replace('.', '*').replace('0', '*')
@@ -25,10 +42,14 @@ def test_performance(filename: str) -> None:
             print(f'Input puzzle:\n{puzzle}')
             print(f'\nSolved in {end - start:.4f} seconds.\n')
             print(f'{solution}\n')
+    speed_end = measure_cpu_speed(5)
+    unstable = max(speed_begin, speed_end) / min(speed_begin, speed_end) > 1.10
     result = f'Puzzles solved: {len(times)}\n' \
-        f'Total time taken: {sum(times)}\n' \
-        f'Average time per puzzle: {sum(times) / len(times)}\n' \
-        f'Max time: {max(times)}\n'
+             f'Total time taken: {sum(times)}\n' \
+             f'Average time per puzzle: {sum(times) / len(times)}\n' \
+             f'Max time: {max(times)}\n' \
+             f'Processor speed: {speed_begin} ({"unstable" if unstable else "stable"})\n' \
+             f'Relative performance: {(speed_begin / 100_000) / (sum(times) / len(times))}\n'
 
     with open('progress.txt', 'a') as log:
         log.write(f'{str(datetime.now())[:-7]} (File: {filename})')
